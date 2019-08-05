589f3e18125fb3d73619dd237b7f843c4614c6ed
sunwucode <wukongkhan8@gmail.com>
2019-08-05 16:17:33 +0200

    all routes ok, not tested though

--- Routes/todos.js
+++ Routes/todos.js
@@ -21,4 +21,31 @@ router.post("/", (req, res)=>{
   })
 })
 
+router.get("/:todoId", function (req, res){
+  db.Todo.findbyId(req.params.todoId)
+  .then(function(foundTodo){
+    res.json(foundTodo);
+  })
+  .catch((err)=>{console.log(err);
+  })
+})
+
+router.put("/:todoId", function(req, res){
+  db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+  .then(function(todo){
+    res.json(todo);
+  })
+  .catch((err)=>{console.log(err);
+  })
+})
+
+router.delete("/:todoId", function(req, res){
+  db.Todo.remove({_id: req.params.todoId},)
+  .then(function(){
+    res.json("todo deleted");
+  })
+  .catch((err)=>{console.log(err);
+  })
+})
+
 module.exports = router;
